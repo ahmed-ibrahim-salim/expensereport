198114c4e76cb4e98027bfd16e32f39d96fd5af0
Ahmed Ibrahim <ahmed.ibrahim@efreshli.com>
2024-10-01 09:57:11 +0300

    covered with tests

--- expensereport-swift/Sources/ExpenseReport/ExpenseReport.swift
+++ expensereport-swift/Sources/ExpenseReport/ExpenseReport.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+// switch case
+// magic numbers
+// primitive obsession
+
 enum ExpenseType {
     case breakfast
     case dinner
@@ -12,28 +16,36 @@ struct Expense {
 }
 
 class ExpenseReport {
-    func printReport(expenses: [Expense]) {
+    func doSomethingWithExpenses(_ expense: Expense, _ mealExpenses: inout Int, _ total: inout Int) {
+        if expense.type == .dinner || expense.type == .breakfast {
+            mealExpenses += expense.amount
+        }
+
+        var expenseName = ""
+        switch expense.type {
+        case .breakfast: expenseName = "Breakfast"
+        case .dinner: expenseName = "Dinner"
+        case .carRental: expenseName = "Car Rental"
+        }
+
+        let mealOverExpensesMarker = expense.type == .dinner && expense.amount > 5000 || expense.type == .breakfast && expense.amount > 1000 ? "X" : " "
+        print("\(expenseName)\t\(expense.amount)\t\(mealOverExpensesMarker)")
+
+        total += expense.amount
+    }
+
+    func printReport(expenses: [Expense], date: Date = Date()) {
         var mealExpenses = 0
         var total = 0
-        print("Expense Report \(Date())")
+        print("Expense Report \(date)")
         for expense in expenses {
-            if (expense.type == .dinner || expense.type == .breakfast) {
-                mealExpenses += expense.amount
-            }
-
-            var expenseName = ""
-            switch expense.type {
-            case .breakfast: expenseName = "Breakfast"
-            case .dinner: expenseName = "Dinner"
-            case .carRental: expenseName = "Car Rental"
-            }
-
-            let mealOverExpensesMarker = expense.type == .dinner && expense.amount > 5000 || expense.type == .breakfast && expense.amount > 1000 ? "X" : " "
-            print("\(expenseName)\t\(expense.amount)\t\(mealOverExpensesMarker)")
-
-            total += expense.amount
+            doSomethingWithExpenses(expense, &mealExpenses, &total)
         }
         print("Meal Expenses: \(mealExpenses)")
         print("Total Expenses: \(total)")
+    }
+
+    func print(_ line: String) {
+        Swift.print(line)
     }
 }
--- expensereport-swift/Tests/ExpenseReportTests/ExpenseReportTests.swift
+++ expensereport-swift/Tests/ExpenseReportTests/ExpenseReportTests.swift
@@ -3,5 +3,46 @@ import XCTest
 
 final class ExpenseReportTests: XCTestCase {
     func testPrintReport() throws {
+        let date = makeDate(year: 1, month: 1, day: 1, hour: 1, minute: 1, second: 1)
+        let expenses = [
+            Expense(type: .breakfast, amount: 100),
+            Expense(type: .breakfast, amount: 100_000),
+            Expense(type: .carRental, amount: 5000),
+            Expense(type: .carRental, amount: 5_000_000),
+            Expense(type: .dinner, amount: 100),
+            Expense(type: .dinner, amount: 5_000_000),
+        ]
+        let expensesReport = TestableExpenseReport()
+        expensesReport.printReport(expenses: expenses, date: date)
+
+        let output = """
+        Expense Report 2002-02-01 17:31:01 +0000
+        Breakfast	100	 
+        Breakfast	100000	X
+        Car Rental	5000	 
+        Car Rental	5000000	 
+        Dinner	100	 
+        Dinner	5000000	X
+        Meal Expenses: 5100200
+        Total Expenses: 10105200
+        """
+
+        XCTAssertEqual(output, expensesReport.getLinesAsString())
+    }
+}
+
+extension ExpenseReportTests {
+    func makeDate(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) -> Date { Date(timeIntervalSinceReferenceDate: TimeInterval(year * 31_557_600 + month * 2_629_800 + day * 86400 + hour * 3600 + minute * 60 + second))
+    }
+}
+
+class TestableExpenseReport: ExpenseReport {
+    private var lines: [String] = []
+    override func print(_ line: String) {
+        lines.append(line)
+    }
+
+    func getLinesAsString() -> String {
+        lines.joined(separator: "\n")
     }
 }
